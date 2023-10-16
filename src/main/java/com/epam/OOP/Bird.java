20b2bd703612caa49d488fc31704b8bdf4ec7f48
VG0LD <117583256+VG0LD@users.noreply.github.com>
2023-10-16 20:56:01 +0600

    Update Bird.java

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,10 @@
-package com.epam.OOP;
+public class Bird extends Animal {
+    public Bird() {
+        super("blue", 2, false);
+    }
 
-public class Bird {
-
+    @Override
+    public String getDescription() {
+        return super.getDescription() + " Moreover, it has 2 wings and can fly.";
+    }
 }
