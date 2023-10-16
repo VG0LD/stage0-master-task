60af2113fc158f4dbbe34c347898b12c29148a7b
VG0LD <117583256+VG0LD@users.noreply.github.com>
2023-10-16 21:00:52 +0600

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -1,8 +1,16 @@
 package com.epam.langSyntax;
 
 public class DigitsSumCalculator {
-
     public void calculateSum(int number) {
-    }
+        // Extract the individual digits
+        int digit1 = number % 10;
+        int digit2 = (number / 10) % 10;
+        int digit3 = (number / 100) % 10;
+        int digit4 = number / 1000;
 
+        // Calculate the sum of the digits
+        int sum = digit1 + digit2 + digit3 + digit4;
+        System.out.println("Sum of digits: " + sum);
+    }
 }
+
