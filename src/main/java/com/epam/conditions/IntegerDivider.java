cd5034fea75ca830cc50fe9bcc626d377987f5b1
VG0LD <117583256+VG0LD@users.noreply.github.com>
2023-10-17 12:11:27 +0600

    Update IntegerDivider.java

--- src/main/java/com/epam/conditions/IntegerDivider.java
+++ src/main/java/com/epam/conditions/IntegerDivider.java
@@ -1,8 +1,15 @@
 package com.epam.conditions;
 
 public class IntegerDivider {
-
     public void printCompletelyDivided(int dividend, int divider) {
+        if (divider != 0) {
+            if (dividend % divider == 0) {
+                System.out.println("Can be divided completely");
+            } else {
+                System.out.println("Cannot be divided completely");
+            }
+        } else {
+            System.out.println("Division by zero");
+        }
     }
-
 }
